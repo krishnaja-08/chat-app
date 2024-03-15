936e9e775e31594ca8af9d253553250c466b81cc
krishnaja-08 <akulakrishnaja1614@gmail.com>
2024-03-15 15:36:33 +0530

    Code for the web socket server for the application

---
+++ index.js
@@ -0,0 +1,33 @@
+const net = require('net');
+const sockets=[]
+const server = net.createServer((socket) => {
+  sockets.push(socket)
+  const index=sockets.indexOf(socket)
+    
+  console.log(`Client ${index} is connected`);
+
+  socket.on('data', (data) => {
+    console.log(`Received data from client: ${data}`);
+
+    for (let i = 0; i < sockets.length; i++) {
+      if(socket!=sockets[i]){
+        sockets[i].write(data);
+     
+      }
+      
+  }
+    
+  });
+
+  
+  socket.on('end', () => {
+    sockets = sockets.filter(item => item != socket);
+    console.log('Client disconnected');
+  });
+});
+const port = 3000;
+
+
+server.listen(port, () => {
+  console.log(`Server is listening on port ${server.address().port}`);
+});
